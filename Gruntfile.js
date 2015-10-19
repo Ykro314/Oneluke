2926152635fa6f59bb07a96a49f53968b40e743c
Ykro314 <lolkokpol@gmal.com>
2015-10-20 00:53:34 +0300

    Финальная версия, с префиксами

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+    
+//    grunt.loadNpmTasks("grunt-autoprefixer");
+//    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    
+    require("load-grunt-tasks")(grunt);
+    
+    grunt.initConfig({
+        autoprefixer:  {
+            options: {
+                browsers: ["last 3 version","ie 10"]
+            },
+            style: {
+                src: "css/style.css"
+            }
+        },
+        cssmin: {
+            style: {
+                files: {
+                    "css/style.min.css": ["css/style.css"]
+                }
+            }
+        }
+    });
+    
+    grunt.registerTask("build", [
+        "autoprefixer",
+        "cssmin"
+    ]);
+};
